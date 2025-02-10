cee41cd15c08bb40a57c19f5fff37a6bd003d09a
ryuto-alt <146914455+ryuto-alt@users.noreply.github.com>
2025-02-10 18:05:53 +0900

    完成

--- RailCamera.cpp
+++ RailCamera.cpp
@@ -25,7 +25,7 @@ void RailCamera::Update()
 	//ワールドトランスフォームの座標の数値を加算したりする(移動)
 	worldTransform_.translation_.x += 0.1f;
 	//ワールドトランスフォームの角度の数値を加算したりする(回転)
-	worldTransform_.rotation_.y += 0.005f;
+	worldTransform_.rotation_.y += 0.005f; 
     //ワールドトランスフォームのワールド行列再計算
 	worldTransform_.NoTransferUpdateMatrix();
 
